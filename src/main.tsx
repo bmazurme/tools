40c11349746681d6c7693019dc0fa06c5687cf95
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-14 15:54:15 +0300

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import App from './App.tsx'
+import App from './app.tsx'
 import { store } from './store/index.ts'
 
-import './index.css'
+
+import './index.css';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
