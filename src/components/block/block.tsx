ec40b298a9a98f708217ed00297ce600b2e78b36
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-15 15:58:49 +0300

    fix: block

---
+++ src/components/block/block.tsx
@@ -0,0 +1,25 @@
+import { Button, Icon, TextInput } from '@gravity-ui/uikit';
+import { SquareMinus } from '@gravity-ui/icons';
+
+export default function Block({ action, value }: { action: () => void; value: string }) {
+  return (
+      <div className="block_header">
+        <TextInput
+          placeholder="Placeholder"
+          size="m"
+          value={value}
+        />
+        <Button
+          view="outlined"
+          size="m"
+          onClick={action}
+          title="Удалить блок"
+        >
+          <Icon
+            data={SquareMinus}
+            size={18}
+          />
+        </Button>
+      </div>
+  );
+}
