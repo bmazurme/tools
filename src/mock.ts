41e36b76abc1f9a18487a1c76478021f63a35b6d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-16 10:28:29 +0300

    add: mock

---
+++ src/mock.ts
@@ -0,0 +1,33 @@
+const menuDocuments = [
+  {
+    link: 'documents/add',
+    name: 'Document add',
+  },
+  {
+    link: 'document/1/rain-roof',
+    name: 'Document 1',
+  },
+  {
+    link: 'document/2/rain-roof',
+    name: 'Document 2',
+  },
+  {
+    link: 'document/2/rain-runoff',
+    name: 'Document 3',
+  },
+];
+const menuProjects = [
+  {
+    link: '/project/1',
+    name: 'Project 1',
+  },
+  {
+    link: '/project/2',
+    name: 'Project 2',
+  },
+  {
+    link: '/project/2',
+    name: 'Project 2',
+  },
+];
+export { menuProjects, menuDocuments };
