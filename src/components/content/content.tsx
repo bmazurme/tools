ad1576c1e9fce87e946b52839e9a1bd106c1f6f9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-16 10:28:50 +0300

    fix: content

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,5 +1,8 @@
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+import { Button, Icon } from '@gravity-ui/uikit';
+import { FolderOpen, Moon, Sun } from '@gravity-ui/icons';
+import NavigationBreadcrumbs from '../NavigationBreadcrumbs';
 
 import type { PropsWithChildren } from 'react';
 
@@ -18,15 +21,41 @@ export default function Content({ children, menu, sidebar }: PropsWithChildren &
     <div className={style.page}>
       {sidebar && 
         <div className={style.sidebar}>
-          <Link to="/projects" className={style.logo}>
-            top
-          </Link>
-          <div className={style.menu}>
-            {menu.map(({ link, name }) => <Link key={uuidv4()} to={link}>{name}</Link>)}
+          <div className={style.sidebar_content}>
+            <Link to="/projects" className={style.logo}>
+              <FolderOpen />
+            </Link>
+
+            <div className={style.menu}>
+              {menu.map(({ link, name }) => <Link key={uuidv4()} to={link}>{name}</Link>)}
+            </div>
           </div>
         </div>
       }
-      {children}
+      <div className="main">
+        <div className={style.header}>
+          <div className={style.bar}>
+            <div className={style.bread}>
+              <div className={style.bread_t}>
+                <div className={style.bread_b}><NavigationBreadcrumbs /></div>
+              </div>
+            </div>
+
+
+              <div>
+                <Button view="normal" size="s" pin="round-clear" selected>
+                  <Icon data={Sun} size={16} />
+                </Button>
+                <Button view="normal" size="s" pin="clear-round">
+                  <Icon data={Moon} size={16} />
+                </Button>
+              </div>
+            </div>
+          </div>
+        <div className={style.board}>
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
