d9920a533e16e60f0bdc2e05333d69238e968d4d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-16 10:24:32 +0300

    fix: project-add-page

--- src/pages/project-add-page/project-add-page.tsx
+++ src/pages/project-add-page/project-add-page.tsx
@@ -1,11 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import { Button, Icon, Text, TextInput } from '@gravity-ui/uikit';
+import { ArrowLeft } from '@gravity-ui/icons';
+
 import Content from '../../components/content/content';
 
+import style from './project-add-page.module.css';
+
 export default function ProjectAddPage() {
+  const navigate = useNavigate();
+  
   return (
     <Content menu={[]} sidebar>
-      <div>
-        <div>
-          ProjectAdd title
+      <div className="content">
+        <Button view="flat" size="l" onClick={() => navigate(-1)}>
+          <Icon data={ArrowLeft} size={18} />
+          Назад
+        </Button>
+        <Text variant="header-1">Добавить проект</Text>
+        <TextInput placeholder="Placeholder" label="Название" size="l" />
+        <TextInput placeholder="Placeholder" label="Адрес" size="l" />
+        <TextInput placeholder="Placeholder" label="Описание" size="l" />
+        <div className={style.buttons}>
+          <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>Сохранить</Button>
+          <Button view="flat" size="l" onClick={() => navigate('/projects')}>Отменить</Button>
         </div>
       </div>
     </Content>
