efc297bf0124e2117ec9a6de799938c0f473cb25
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-16 10:23:48 +0300

    fix: projects-page

--- src/pages/projects-page/projects-page.tsx
+++ src/pages/projects-page/projects-page.tsx
@@ -1,30 +1,64 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Button, Icon, Pagination, type PaginationProps, Table, type TableActionConfig, type TableDataItem, withTableActions } from '@gravity-ui/uikit';
+import { Pencil, Plus, TrashBin } from '@gravity-ui/icons';
+
 import Content from '../../components/content/content';
 
-export default function ProjectsPage() {
-  const menu = [
+import { menuProjects } from '../../mock';
+
+import style from './projects-page.module.css';
+
+const MyTable = withTableActions(Table);
+const data = [
+  { id: 1, text: 'Hello' },
+  { id: 2, text: 'World' },
+];
+const columns = [{ id: 'id' }, { id: 'text' }];
+
+const getRowActions = (_item: TableDataItem, _index: number): TableActionConfig<TableDataItem>[] => {
+  return [
     {
-      link: '/project/add',
-      name: 'Project add',
+      text: 'Редактировать',
+      handler: () => {},
+      theme: 'normal',
+      icon: <Icon data={Pencil} size={18} />,
     },
     {
-      link: '/project/1',
-      name: 'Project 1',
-    },
-    {
-      link: '/project/2',
-      name: 'Project 2',
-    },
-    {
-      link: '/project/2',
-      name: 'Project 2',
+      text: 'Удалить',
+      handler: () => {},
+      theme: 'danger',
+      icon: <Icon data={TrashBin} size={18} />,
     },
   ];
+};
+
+export default function ProjectsPage() {
+  const navigate = useNavigate();
+  const { pageId } = useParams();
+  const [state, setState] = React.useState({ page: 1, pageSize: 100 });
+
+  const handleUpdate: PaginationProps['onUpdate'] = (page, pageSize) =>
+    setState((prevState) => ({ ...prevState, page, pageSize }));
 
   return (
-    <Content menu={menu} sidebar>
-      ProjectsPage
-      <div>
-        Profile data
+    <Content menu={menuProjects} sidebar>
+      <div className="content">
+        {/* <Outlet /> */}
+        <div className={style.menu}>
+          {menuProjects.map(({ link, name }) => <Link key={uuidv4()} to={link}>{name}</Link>)}
+        </div>
+
+        <Button view="action" size="l" onClick={() => navigate('add')}>
+          <Icon data={Plus} size={18} />
+            Добавить проект
+        </Button>
+
+        <MyTable data={data} columns={columns} getRowActions={getRowActions} />
+
+        {state.page} - {state.pageSize} - {pageId}
+        <Pagination page={1} pageSize={100} total={1000} onUpdate={handleUpdate} />
       </div>
     </Content>
   );
