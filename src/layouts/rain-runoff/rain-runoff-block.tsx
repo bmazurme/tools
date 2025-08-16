3ecccbfc67c01f3fc56fd5644eed52e5b7586f0d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-16 10:34:59 +0300

    fix: rain-runoff-block

--- src/layouts/rain-runoff/rain-runoff-block.tsx
+++ src/layouts/rain-runoff/rain-runoff-block.tsx
@@ -2,60 +2,60 @@ import { useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDrag, useDrop } from 'react-dnd';
 
+import Block from '../../components/block/block';
 import Column from './rain-runoff-column';
 import Item from './rain-runoff-item';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { movedRainRoofBlock, rainRoofsSelector } from '../../store';
+import { movedRainRunoffBlock, rainRunoffsSelector, removeRainRunoffBlock } from '../../store';
 import { TARGET_TYPE } from '../../config';
 
-export default function Block({
-  block,
-  index,
-}: {
-  block: BlockType;
-  index: number;
-}) {
+export default function RainRunoffBlock({ block, index }: { block: BlockType; index: number }) {
   const dispatch = useAppDispatch();
-  const { blocks } = useAppSelector(rainRoofsSelector);
+  const { blocks } = useAppSelector(rainRunoffsSelector);
   const ref = useRef<HTMLDivElement>(null);
 
   const moveBlockHandler = async (dragIndex: number, hoverIndex: number) => {
     const dragItem = blocks[dragIndex];
 
     if (dragItem) {
-      dispatch(movedRainRoofBlock({ hoverIndex, dragIndex, dragItem }));
+      dispatch(movedRainRunoffBlock({ hoverIndex, dragIndex, dragItem }));
     }
   };
 
   const [, drop] = useDrop({
     accept: TARGET_TYPE.BLOCKS,
-    hover(itm: { index: number }, monitor) {
+    hover(item: { index: number }, monitor) {
       if (!ref.current) {
         return;
       }
-      const dragIndex = itm.index!;
+
+      const dragIndex = item.index;
       const hoverIndex = index;
 
       if (dragIndex === hoverIndex) {
         return;
       }
+
       const hoverBoundingRect = ref.current.getBoundingClientRect();
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      const clientOffset = monitor.getClientOffset()!;
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
 
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
+      if (clientOffset?.y) {
+        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+          return;
+        }
+
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+          return;
+        }
+
+        moveBlockHandler(dragIndex, hoverIndex);
+        // eslint-disable-next-line no-param-reassign
+        item.index = hoverIndex;
       }
-
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-
-      moveBlockHandler(dragIndex, hoverIndex);
-      itm.index = hoverIndex;
     },
   });
 
@@ -67,14 +67,19 @@ export default function Block({
     }),
   });
 
-  const returnItemsForColumn = (block: BlockType) => {
-    return block.items.map((item: ItemType, idx: number) => (
-      <Item key={uuidv4()} index={idx} item={item} />
+  const returnItemsForColumn = (items: ItemType[]) => {
+    return items.map((item: ItemType, idx: number) => (
+      <Item
+        key={uuidv4()}
+        index={idx}
+        item={item}
+      />
     ));
   };
 
   const opacity = isDragging ? 0.4 : 1;
   const border = isDragging ? 'solid 1px var(--table-cell)' : 'none';
+  const onHandleRemoveRunoffBlock = () => dispatch(removeRainRunoffBlock({ blockId: block.id }));
 
   drag(drop(ref));
 
@@ -84,8 +89,10 @@ export default function Block({
       style={{ opacity, border, borderRadius: '8px' }}
       className="block"
     >
-      {`block_${block.id}`}
-      <Column blockId={block.id}>{returnItemsForColumn(block)}</Column>
+      <Block action={onHandleRemoveRunoffBlock} value={`block_${block.id}`}/>
+      <Column blockId={block.id}>
+        {returnItemsForColumn(block.items)}
+      </Column>
     </div>
   );
 }
