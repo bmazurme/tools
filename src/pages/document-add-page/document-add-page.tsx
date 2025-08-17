34eafe953fc369a078127335ca54118078f253ae
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-17 13:33:05 +0300

    fix: document-add-page

--- src/pages/document-add-page/document-add-page.tsx
+++ src/pages/document-add-page/document-add-page.tsx
@@ -1,25 +1,35 @@
 import { useNavigate } from 'react-router-dom';
-import { Button, Icon, Text, TextInput } from '@gravity-ui/uikit';
+import { Button, Icon, Select, Text, TextInput } from '@gravity-ui/uikit';
 import { ArrowLeft } from '@gravity-ui/icons';
 
+import Content from '../../components/content/content';
 import style from './document-add-page.module.css';
 
 export default function DocumentAddPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="content">
-      <Button view="flat" size="l" onClick={() => navigate(-1)}>
-        <Icon data={ArrowLeft} size={18} />
-        Назад
-      </Button>
-      <Text variant="header-1">Добавить документ</Text>
-      <TextInput placeholder="Placeholder" label="Название" size="l" />
-      <TextInput placeholder="Placeholder" label="Описание" size="l" />
-      <div className={style.buttons}>
-        <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>Сохранить</Button>
-        <Button view="flat" size="l" onClick={() => navigate('/projects')}>Отменить</Button>
+    <Content menu={[]} sidebar>
+      <div className="content">
+        <Button view="flat" size="m" onClick={() => navigate(-1)}>
+          <Icon data={ArrowLeft} size={18} />
+          Назад
+        </Button>
+        <Text variant="header-1">Добавить документ</Text>
+        <TextInput placeholder="Placeholder" label="Название" size="l" />
+        <Select
+          placeholder="Custom filter"
+          size="l"
+          width="max"
+        >
+          <Select.Option value="val_1">Value 1</Select.Option>
+        </Select>
+        <TextInput placeholder="Placeholder" label="Описание" size="l" />
+        <div className={style.buttons}>
+          <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>Сохранить</Button>
+          <Button view="flat" size="l" onClick={() => navigate('/projects')}>Отменить</Button>
+        </div>
       </div>
-    </div>
+    </Content>
   );
 }
