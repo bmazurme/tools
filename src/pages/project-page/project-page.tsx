40cb9303c6c0eada3186e6a2297eae33c7d2c31f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-17 13:32:03 +0300

    fix: project-page

--- src/pages/project-page/project-page.tsx
+++ src/pages/project-page/project-page.tsx
@@ -1,18 +1,26 @@
-import { Outlet } from 'react-router-dom';
-import { Text } from '@gravity-ui/uikit';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Button, Icon, Text } from '@gravity-ui/uikit';
+import { ArrowLeft } from '@gravity-ui/icons';
 
 import Content from '../../components/content/content';
 
-import { menuDocuments } from '../../mock';
-
 import style from './project-page.module.css';
 
-export default function ProjectPage() {  
+export default function ProjectPage() {
+  const navigate = useNavigate();
+  
   return (
-    <Content menu={menuDocuments} sidebar>
-      <div className={style.content}>
-        <Text variant="header-1">Project Page</Text>
-        <Outlet />
+    <Content menu={[]} sidebar>
+      <div className="content">
+        <Button view="flat" size="m" onClick={() => navigate(-1)}>
+          <Icon data={ArrowLeft} size={18} />
+          Назад
+        </Button>
+        <div className={style.main}>
+          <Text variant="header-1">Project Page</Text>
+          <Outlet />
+        </div>
+
       </div>
     </Content>
   );
