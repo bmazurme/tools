1a48c3a827fa6825576ef68b538fe56404db4517
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-17 14:19:46 +0300

    fix: project

---
+++ src/layouts/project/project.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { Text } from '@gravity-ui/uikit';
+
+import Content from '../../components/content/content';
+
+import style from './project.module.css';
+
+export default function Project() {  
+  return (
+    <Content menu={[]} sidebar>
+      <div className={style.content}>
+        <Text variant="header-1">Project Layout</Text>
+        <Outlet />
+      </div>
+    </Content>
+  );
+}
