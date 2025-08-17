be1afcff08967b52fd4a69631c9d6ab1450ea90c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-17 13:31:37 +0300

    fix: projects-page

--- src/pages/projects-page/projects-page.tsx
+++ src/pages/projects-page/projects-page.tsx
@@ -1,64 +1,63 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Button, Icon, Pagination, type PaginationProps, Table, type TableActionConfig, type TableDataItem, withTableActions } from '@gravity-ui/uikit';
 import { Pencil, Plus, TrashBin } from '@gravity-ui/icons';
 
 import Content from '../../components/content/content';
 
-import { menuProjects } from '../../mock';
-
 import style from './projects-page.module.css';
 
 const MyTable = withTableActions(Table);
 const data = [
-  { id: 1, text: 'Hello' },
-  { id: 2, text: 'World' },
+  { id: 1, text: 'Проект 1', },
+  { id: 2, text: 'Проект 2', },
+  { id: 3, text: 'Проект 3', },
 ];
-const columns = [{ id: 'id' }, { id: 'text' }];
-
-const getRowActions = (_item: TableDataItem, _index: number): TableActionConfig<TableDataItem>[] => {
-  return [
-    {
-      text: 'Редактировать',
-      handler: () => {},
-      theme: 'normal',
-      icon: <Icon data={Pencil} size={18} />,
-    },
-    {
-      text: 'Удалить',
-      handler: () => {},
-      theme: 'danger',
-      icon: <Icon data={TrashBin} size={18} />,
-    },
-  ];
-};
+const columns = [
+  { id: 'id', name: '#', width: 60 },
+  { id: 'text', name: 'Название проекта', width: 260 },
+];
 
 export default function ProjectsPage() {
   const navigate = useNavigate();
-  const { pageId } = useParams();
+  const { projectPage } = useParams();
   const [state, setState] = React.useState({ page: 1, pageSize: 100 });
+
+  const getRowActions = (item: TableDataItem, _index: number): TableActionConfig<TableDataItem>[] => {
+    return [
+      {
+        text: 'Редактировать',
+        handler: () => navigate(`/project/${item.id}`),
+        theme: 'normal',
+        icon: <Icon data={Pencil} size={18} />,
+      },
+      {
+        text: 'Удалить',
+        handler: () => {},
+        theme: 'danger',
+        icon: <Icon data={TrashBin} size={18} />,
+      },
+    ];
+  };
 
   const handleUpdate: PaginationProps['onUpdate'] = (page, pageSize) =>
     setState((prevState) => ({ ...prevState, page, pageSize }));
 
   return (
-    <Content menu={menuProjects} sidebar>
+    <Content menu={[]} sidebar>
       <div className="content">
-        {/* <Outlet /> */}
-        <div className={style.menu}>
-          {menuProjects.map(({ link, name }) => <Link key={uuidv4()} to={link}>{name}</Link>)}
-        </div>
-
-        <Button view="action" size="l" onClick={() => navigate('add')}>
+        <Button view="action" size="m" onClick={() => navigate('add')}>
           <Icon data={Plus} size={18} />
-            Добавить проект
+          Добавить проект
         </Button>
 
-        <MyTable data={data} columns={columns} getRowActions={getRowActions} />
+        <div className={style.main}>
+          <MyTable data={data} columns={columns} getRowActions={getRowActions} />
 
-        {state.page} - {state.pageSize} - {pageId}
-        <Pagination page={1} pageSize={100} total={1000} onUpdate={handleUpdate} />
+          {state.page} - {state.pageSize} - {projectPage}
+          <Pagination page={1} pageSize={100} total={1000} onUpdate={handleUpdate} />
+        </div>
+
       </div>
     </Content>
   );
