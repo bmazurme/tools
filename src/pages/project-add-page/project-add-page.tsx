736ad9b2bd662a6fef33cbd84f951c6b02ad7aff
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-18 18:37:25 +0300

    fix: project-add-page

--- src/pages/project-add-page/project-add-page.tsx
+++ src/pages/project-add-page/project-add-page.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
-import { Button, Icon, Text, TextInput } from '@gravity-ui/uikit';
+import {
+  Button, Icon, Text, TextInput,
+} from '@gravity-ui/uikit';
 import { ArrowLeft } from '@gravity-ui/icons';
 
 import Content from '../../components/content/content';
@@ -8,9 +10,9 @@ import style from './project-add-page.module.css';
 
 export default function ProjectAddPage() {
   const navigate = useNavigate();
-  
+
   return (
-    <Content menu={[]} sidebar>
+    <Content sidebar>
       <div className="content">
         <Button view="flat" size="m" onClick={() => navigate(-1)}>
           <Icon data={ArrowLeft} size={18} />
@@ -21,8 +23,12 @@ export default function ProjectAddPage() {
         <TextInput placeholder="Placeholder" label="Адрес" size="l" />
         <TextInput placeholder="Placeholder" label="Описание" size="l" />
         <div className={style.buttons}>
-          <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>Сохранить</Button>
-          <Button view="flat" size="l" onClick={() => navigate('/projects')}>Отменить</Button>
+          <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>
+            Сохранить
+          </Button>
+          <Button view="flat" size="l" onClick={() => navigate('/projects')}>
+            Отменить
+          </Button>
         </div>
       </div>
     </Content>
