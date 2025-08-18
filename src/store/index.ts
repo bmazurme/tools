4f633b145cdb4b60528df313aa7f926fc0baabae
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-18 18:33:52 +0300

    fix: store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import documentsReducer from './slices/documents-slice';
+import projectsReducer from './slices/projects-slice';
+import themeReducer from './slices/theme-slice';
+
 import rainRoofsReducer from './slices/rain-roofs-slice';
 import rainRunoffsReducer from './slices/rain-runoffs-slice';
 
@@ -7,10 +11,13 @@ export * from './slices/index';
 
 export const store = configureStore({
   reducer: {
+    documents: documentsReducer,
+    projects: projectsReducer,
     rainRoofs: rainRoofsReducer,
     rainRunoffs: rainRunoffsReducer,
+    theme: themeReducer,
   },
-})
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
