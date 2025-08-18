664d06b59361369cc734e09d27dfa9ab030277cf
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-18 18:38:02 +0300

    fix: document-add-page

--- src/pages/document-add-page/document-add-page.tsx
+++ src/pages/document-add-page/document-add-page.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
-import { Button, Icon, Select, Text, TextInput } from '@gravity-ui/uikit';
+import {
+  Button, Icon, Select, Text, TextInput,
+} from '@gravity-ui/uikit';
 import { ArrowLeft } from '@gravity-ui/icons';
 
 import Content from '../../components/content/content';
@@ -9,7 +11,7 @@ export default function DocumentAddPage() {
   const navigate = useNavigate();
 
   return (
-    <Content menu={[]} sidebar>
+    <Content sidebar>
       <div className="content">
         <Button view="flat" size="m" onClick={() => navigate(-1)}>
           <Icon data={ArrowLeft} size={18} />
@@ -22,12 +24,18 @@ export default function DocumentAddPage() {
           size="l"
           width="max"
         >
-          <Select.Option value="val_1">Value 1</Select.Option>
+          <Select.Option value="val_1">
+            Value 1
+          </Select.Option>
         </Select>
         <TextInput placeholder="Placeholder" label="Описание" size="l" />
         <div className={style.buttons}>
-          <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>Сохранить</Button>
-          <Button view="flat" size="l" onClick={() => navigate('/projects')}>Отменить</Button>
+          <Button view="action" size="l" onClick={() => navigate('/project/2/document/1/rain-roof')}>
+            Сохранить
+          </Button>
+          <Button view="flat" size="l" onClick={() => navigate('/projects')}>
+            Отменить
+          </Button>
         </div>
       </div>
     </Content>
