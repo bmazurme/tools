0ae99bb8068054b792e893b7e6685b62c27a424f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-19 19:18:29 +0300

    fix: documents-slice

--- src/store/slices/documents-slice.ts
+++ src/store/slices/documents-slice.ts
@@ -2,14 +2,10 @@ import { type PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 import type { RootState } from '..';
 
-type DocumentType = {
+export type DocumentType = {
   id: number;
   name: string;
-  type: {
-    id: number;
-    name: string;
-    link: string;
-  };
+  type: string;
 };
 
 type DocumentsState = {
@@ -20,11 +16,7 @@ export const initialStateDocuments: DocumentsState = {
   data: Array.from({ length: 21 }, (_x, i) => ({
     id: i,
     name: `Документ ${i}`,
-    type: {
-      id: i % 2 ? 1 : 0,
-      name: i % 2 ? 'WS' : 'S',
-      link: i % 2 ? 'rain-roof' : 'rain-runoff',
-    },
+    type: i % 2 ? 'rain-roof' : 'rain-runoff',
   })),
 };
 
