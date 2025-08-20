3eec0b9cc5120e8b23b67e2bb4abcc15128084a0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-20 09:18:47 +0300

    fix: documents-page

--- src/pages/documents-page/documents-page.tsx
+++ src/pages/documents-page/documents-page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   Button, Icon, Pagination,
   type PaginationProps, Table, type TableActionConfig, type TableDataItem,
@@ -10,40 +10,33 @@ import {
 } from '@gravity-ui/icons';
 
 import Content from '../../components/content/content';
-import { useAppSelector } from '../../hooks';
-import { documentsSelector } from '../../store';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { documentsSelector, removeDocument } from '../../store';
 
 const MyTable = withTableActions(Table);
 const columns = [
   { id: 'id', name: '#', width: 60 },
   { id: 'name', name: 'Название документа', width: '100%' },
-  { id: 'type.name', name: 'Тип документа', width: 160 },
+  { id: 'type', name: 'Тип документа', width: 160 },
 ];
 
 export default function DocumentsPage() {
+  const dispatch = useAppDispatch();
   const documents = useAppSelector(documentsSelector);
   const navigate = useNavigate();
-  const { projectPage } = useParams();
-  const [state, setState] = React.useState({ page: 1, pageSize: 100 });
-
+  const [state, setState] = React.useState({ page: 1, pageSize: 10 });
   // eslint-disable-next-line max-len, @typescript-eslint/no-unused-vars
   const getRowActions = (item: TableDataItem, _index: number): TableActionConfig<TableDataItem>[] => [
-    // {
-    //   text: 'Редактировать',
-    //   handler: () => navigate(`document/${item.id}/${item.type.link}`),
-    //   theme: 'normal',
-    //   icon: <Icon data={Pencil} size={18} />,
-    // },
     {
       text: 'Удалить',
-      handler: () => {},
+      handler: () => dispatch(removeDocument({ id: item.id })),
       theme: 'danger',
       icon: <Icon data={TrashBin} size={18} />,
     },
   ];
 
   const handleUpdate: PaginationProps['onUpdate'] = (page, pageSize) => setState((prevState) => ({ ...prevState, page, pageSize }));
-  const handleRowClick = (rowData: TableDataItem) => navigate(`document/${rowData.id}/${rowData.type.link}`);
+  const handleRowClick = (rowData: TableDataItem) => navigate(`document/${rowData.id}/${rowData.type}`);
 
   return (
     <Content sidebar>
@@ -52,6 +45,7 @@ export default function DocumentsPage() {
           <Icon data={ArrowLeft} size={18} />
           Назад
         </Button>
+
         <Text variant="header-1">Документы</Text>
         <Button view="action" size="m" onClick={() => navigate('add')}>
           <Icon data={Plus} size={18} />
@@ -60,20 +54,21 @@ export default function DocumentsPage() {
 
         <MyTable
           className="table"
-          data={documents}
+          data={documents.slice((state.page - 1) * 10, state.page * 10)}
           columns={columns}
           getRowActions={getRowActions}
           onRowClick={handleRowClick}
         />
-        {state.page}
-        {' '}
-        -
-        {state.pageSize}
-        {' '}
-        -
-        {projectPage}
-        <Pagination page={1} pageSize={100} total={1000} onUpdate={handleUpdate} />
+
+        <Pagination
+          page={state.page}
+          pageSize={state.pageSize}
+          total={documents.length}
+          onUpdate={handleUpdate}
+        />
       </div>
     </Content>
   );
 }
+
+export { MyTable };
