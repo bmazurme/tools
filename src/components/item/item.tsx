f035e892604971f416f635749e34457c035326b9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-21 08:10:35 +0300

    fix: item

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,16 +1,17 @@
 import { Button, Icon } from '@gravity-ui/uikit';
 import { Minus, Pencil } from '@gravity-ui/icons';
 
-export default function Item({ children, action }:
-  { children: string; action: () => void }) {
+export default function Item({ children, removeAction, editAction }:
+  { children: string; removeAction: () => void; editAction: () => void; }) {
   return (
     <>
       { children }
-      <div>
+      <div className="item_tools">
         <Button
           view="outlined"
           size="s"
           title="Редактировать строку"
+          onClick={editAction}
         >
           <Icon
             data={Pencil}
@@ -20,7 +21,7 @@ export default function Item({ children, action }:
         <Button
           view="outlined"
           size="s"
-          onClick={action}
+          onClick={removeAction}
           title="Удалить строку"
         >
           <Icon
