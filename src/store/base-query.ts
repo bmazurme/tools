a30d75006969546cab0fb859350ba4f8b5581a23
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-23 22:33:06 +0300

    fix: base-query

---
+++ src/store/base-query.ts
@@ -0,0 +1,51 @@
+import { fetchBaseQuery } from '@reduxjs/toolkit/query';
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+
+import { toaster } from '../main';
+
+const BASE_PROJECT_API_URL = 'http://localhost:3000';
+
+// Create our baseQuery instance
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_PROJECT_API_URL,
+  prepareHeaders: (headers) => headers,
+  credentials: 'include',
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError | null
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 400) {
+    toaster.add({
+      title: '400',
+      name: '400',
+      theme: 'danger',
+    });
+  } else if (result.error && result.error.status === 401) {
+    toaster.add({
+      title: '401',
+      name: '401',
+      theme: 'danger',
+    });
+  } else if (result.error && result.error.status === 404) {
+    toaster.add({
+      title: 'Not fount 404',
+      name: '404',
+      theme: 'danger',
+    });
+  } else if (result.error && result.error.status === 500) {
+    toaster.add({
+      title: '500',
+      name: '500',
+      theme: 'danger',
+    });
+  }
+
+  return result;
+};
+
+export default baseQueryWithReauth;
