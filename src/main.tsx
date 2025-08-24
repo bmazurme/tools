722ef1d9bc256f8acabdc3f3048ff45130fde174
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-24 10:36:07 +0300

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Toaster, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
 
 import ThemeWrapper from './components/theme-wrapper/theme-wrapper';
 import App from './app';
@@ -11,14 +12,22 @@ import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
 import './styles/index.css';
 
+const toaster = new Toaster();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeWrapper>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <ToasterProvider toaster={toaster}>
+          <BrowserRouter>
+            <App />
+            <ToasterComponent />
+          </BrowserRouter>
+        </ToasterProvider>
       </ThemeWrapper>
     </Provider>
   </StrictMode>,
 );
+
+// eslint-disable-next-line import/prefer-default-export
+export { toaster };
