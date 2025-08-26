cbf7d753d49368c2957fe7ba9c3154166fd47b5b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-26 19:57:00 +0300

    fix: profile-page

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -1,27 +1,60 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Controller, useForm } from 'react-hook-form';
 import {
   Button, Icon, Text, TextInput,
 } from '@gravity-ui/uikit';
 import { ArrowLeft } from '@gravity-ui/icons';
 
 import Content from '../../components/content/content';
-import { useGetUserMeMutation, usersSelector } from '../../store';
-import { useAppSelector } from '../../hooks';
+import { useGetUserMeMutation, useUpdateUserMutation } from '../../store';
+import useUser from '../../hooks/use-user';
+
+type FormPayload = { status: string };
+
+const fields = [
+  {
+    name: 'status',
+    label: 'Статус',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{3,50}$/,
+      message: 'Name is invalid',
+    },
+    autoComplete: 'status',
+  },
+];
 
 export default function ProfilePage() {
   const navigate = useNavigate();
-  const user = useAppSelector(usersSelector);
+  const user = useUser();
   const [getUser] = useGetUserMeMutation();
-  console.log(user);
+  const [updateUser] = useUpdateUserMutation();
+
+  const { control, handleSubmit, reset } = useForm<FormPayload>({
+    defaultValues: {
+      status: user?.status,
+    },
+  });
+
+  const onSubmit = async (data: FormPayload) => {
+    try {
+      await updateUser({ ...user!, status: data.status });
+    } catch (error) {
+      console.error('Ошибка при обновлении проекта:', error);
+    }
+  };
 
   useEffect(() => {
     getUser();
   }, []);
+  useEffect(() => {
+    reset({ status: user?.status });
+  }, [user?.status]);
 
   return (
     <Content sidebar>
-      <div className="content">
+      <form className="content" onSubmit={handleSubmit(onSubmit)}>
         <Button view="flat" size="m" onClick={() => navigate(-1)}>
           <Icon data={ArrowLeft} size={18} />
           Назад
@@ -30,8 +63,26 @@ export default function ProfilePage() {
         <div className="project_main">
           <Text variant="header-1">Профиль</Text>
         </div>
-        <TextInput placeholder="Placeholder" size="l" />
-        <TextInput placeholder="Placeholder" size="l" />
+        {fields.map((input) => (
+          <Controller
+            key={input.name}
+            name={input.name as keyof FormPayload}
+            rules={{
+              pattern: input.pattern,
+              // required: input.required,
+            }}
+            control={control}
+            render={({ field, fieldState }) => (
+              <TextInput
+                {...field}
+                {...input}
+                size="l"
+                type="text"
+                error={fieldState.error?.message}
+              />
+            )}
+          />
+        ))}
 
         <div className="buttons">
           <Button view="action" size="l" type="submit">
@@ -42,7 +93,7 @@ export default function ProfilePage() {
           </Button>
         </div>
 
-      </div>
+      </form>
     </Content>
   );
 }
