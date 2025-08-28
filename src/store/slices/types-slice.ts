9a9ef257848a7bb9282182428fe2a925ef4dc914
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-28 21:49:51 +0300

    fix: types-slice

---
+++ src/store/slices/types-slice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { typesApiEndpoints } from '../api/types-api/endpoints/index';
+
+import type { RootState, Type } from '..';
+
+type TypesState = {
+  data: Type[];
+};
+
+export const initialStateTypes: TypesState = {
+  data: [],
+};
+
+const slice = createSlice({
+  name: 'types',
+  initialState: initialStateTypes,
+  reducers: { },
+
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        typesApiEndpoints.endpoints.getTypes.matchFulfilled,
+        (state, action) => ({ ...state, data: action.payload }),
+      );
+  },
+});
+
+export default slice.reducer;
+export const typesSelector = (state: RootState) => state.types.data;
