8bf7cd17506590041726db8fc8cdba0378307362
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-31 12:42:59 +0300

    fix: document-page

--- src/pages/document-page/document-page.tsx
+++ src/pages/document-page/document-page.tsx
@@ -1,8 +1,72 @@
-import { Outlet } from 'react-router-dom';
+/* eslint-disable react/jsx-props-no-spreading */
+import { useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { Outlet, useParams } from 'react-router-dom';
+import { TextInput } from '@gravity-ui/uikit';
+
+import { useGetDocumentMutation } from '../../store';
+// import { useAppSelector } from '../../hooks';
+import { TEXT_INPUT_PROPS } from '../../config';
+
+type FormPayload = { name: string };
+
+const fields = [
+  {
+    name: 'name',
+    label: 'Название',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{3,50}$/,
+      message: 'Name is invalid',
+    },
+    required: 'Обязательно к заполнению',
+    autoComplete: 'name',
+  },
+];
 
 export default function DocumentPage() {
+  const { id } = useParams();
+  const [getDocument] = useGetDocumentMutation();
+  // const document = useAppSelector(documentSelector);
+
+  const { control, reset } = useForm<FormPayload>({
+    defaultValues: { name: '' },
+  });
+
+  const fetchData = async () => {
+    if (id) {
+      const result = await getDocument(Number(id));
+      reset({ name: result.data?.name });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
+      <form>
+        {fields.map((input) => (
+          <Controller
+            key={input.name}
+            name={input.name as keyof FormPayload}
+            rules={{
+              pattern: input.pattern,
+              required: input.required,
+            }}
+            control={control}
+            render={({ field, fieldState }) => (
+              <TextInput
+                {...field}
+                {...input}
+                {...TEXT_INPUT_PROPS}
+                error={fieldState.error?.message}
+              />
+            )}
+          />
+        ))}
+      </form>
+
       <Outlet />
     </div>
   );
