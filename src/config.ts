f7098d974b4ea520c043b62d0e710ba9fc0ef888
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-31 12:41:21 +0300

    fix: config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { type InputControlSize } from '@gravity-ui/uikit';
+
 const TARGET_TYPE = {
   BLOCKS: 'blocks',
   ITEMS: 'items',
@@ -5,3 +7,19 @@ const TARGET_TYPE = {
 
 // eslint-disable-next-line import/prefer-default-export
 export { TARGET_TYPE };
+
+export const BACK_BUTTON_PROPS = {
+  view: 'flat' as const,
+  size: 'm' as const,
+  // children: (
+  //   <>
+  //     <Icon data={ArrowLeft} size={18} />
+  //     Назад
+  //   </>
+  // ),
+};
+
+export const TEXT_INPUT_PROPS = {
+  size: 'l' as InputControlSize,
+  type: 'text' as const,
+};
