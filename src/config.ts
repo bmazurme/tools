f37a3460461fb2a85fc995348e0a35c014e74dd4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-02 17:50:50 +0300

    fix: config

--- src/config.ts
+++ src/config.ts
@@ -5,18 +5,11 @@ const TARGET_TYPE = {
   ITEMS: 'items',
 };
 
-// eslint-disable-next-line import/prefer-default-export
 export { TARGET_TYPE };
 
 export const BACK_BUTTON_PROPS = {
   view: 'flat' as const,
   size: 'm' as const,
-  // children: (
-  //   <>
-  //     <Icon data={ArrowLeft} size={18} />
-  //     Назад
-  //   </>
-  // ),
 };
 
 export const TEXT_INPUT_PROPS = {
