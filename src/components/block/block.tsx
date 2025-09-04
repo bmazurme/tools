46bc6524c0c44bf1d6a1f9c13a99d40e93a8c35c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-04 18:39:30 +0300

    fix: block

--- src/components/block/block.tsx
+++ src/components/block/block.tsx
@@ -1,25 +1,76 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { Button, Icon, TextInput } from '@gravity-ui/uikit';
-import { SquareMinus } from '@gravity-ui/icons';
+import { TrashBin } from '@gravity-ui/icons';
+import { Controller, useForm } from 'react-hook-form';
+import { TEXT_INPUT_PROPS } from '../../config';
+import { useUpdateBlockMutation } from '../../store';
 
-export default function Block({ action, value }: { action: () => void; value: string }) {
+const fields = [
+  {
+    name: 'name',
+    label: 'Название',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{3,50}$/,
+      message: 'Name is invalid',
+    },
+    required: 'Обязательно к заполнению',
+    autoComplete: 'name',
+  },
+];
+
+type FormPayload = { name: string };
+
+export default function Block({ action, value }
+  : { action: () => void; value: BlockType }) {
+  const [updateBlock] = useUpdateBlockMutation();
+  const { control } = useForm<FormPayload>({
+    defaultValues: { name: value.name },
+  });
+  const onSubmit = async () => {
+    try {
+      if (value.id) {
+        // eslint-disable-next-line no-underscore-dangle
+        const { name } = control._formValues;
+        await updateBlock({ ...value, name });
+      }
+    } catch (error) {
+      console.error('Ошибка при обновлении проекта:', error);
+    }
+  };
+
   return (
-    <div className="block_header">
-      <TextInput
-        placeholder="Placeholder"
-        size="m"
-        value={value}
-      />
+    <form className="block_header">
+      {fields.map((input) => (
+        <Controller
+          key={input.name}
+          name={input.name as keyof FormPayload}
+          rules={{
+            pattern: input.pattern,
+            required: input.required,
+          }}
+          control={control}
+          render={({ field, fieldState }) => (
+            <TextInput
+              {...field}
+              {...input}
+              {...TEXT_INPUT_PROPS}
+              error={fieldState.error?.message}
+              onBlur={onSubmit}
+            />
+          )}
+        />
+      ))}
       <Button
         view="flat"
-        size="m"
+        size="l"
         onClick={action}
         title="Удалить блок"
       >
         <Icon
-          data={SquareMinus}
-          size={18}
+          data={TrashBin}
+          size={20}
         />
       </Button>
-    </div>
+    </form>
   );
 }
