b3b4e1ad76c57cbe75577dad3fc16b1d77606c00
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-04 18:40:46 +0300

    fix: document-page

---
+++ src/pages/document-page/document-page.fields.ts
@@ -0,0 +1,14 @@
+const fields = [
+  {
+    name: 'name',
+    label: 'Название',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{3,50}$/,
+      message: 'Name is invalid',
+    },
+    required: 'Обязательно к заполнению',
+    autoComplete: 'name',
+  },
+];
+
+export default fields;
--- src/pages/document-page/index.ts
+++
@@ -1,4 +0,0 @@
-import withUser from '../../hocs/with-user';
-import DocumentPage from './document-page';
-
-export default withUser(DocumentPage, true) as typeof DocumentPage;
