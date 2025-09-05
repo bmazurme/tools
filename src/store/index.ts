3dbd13de4b0106d8f4ee8772dd2d727b5a881325
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-05 17:40:30 +0300

    fix: store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import {
-  projectsApi, documentsApi, usersApi, typesApi,
+  projectsApi, documentsApi, usersApi, typesApi, blocksApi, rainRoofsApi,
 } from './api/index';
 
 import documentsReducer from './slices/documents-slice';
 import projectsReducer from './slices/projects-slice';
 import usersReducer from './slices/users-slice';
 import typesReducer from './slices/types-slice';
+import blocksReducer from './slices/blocks-slice';
 
 import rainRoofsReducer from './slices/rain-roofs-slice';
-import rainRunoffsReducer from './slices/rain-runoffs-slice';
+// import rainRunoffsReducer from './slices/rain-runoffs-slice';
 
 export * from './api/projects-api/endpoints/index';
 export * from './api/documents-api/endpoints/index';
 export * from './api/users-api/endpoints/index';
 export * from './api/types-api/endpoints/index';
+export * from './api/blocks-api/endpoints/index';
+export * from './api/rain-roofs-api/endpoints/index';
 
 export * from './slices/index';
 
@@ -24,13 +27,16 @@ export const store = configureStore({
     documents: documentsReducer,
     projects: projectsReducer,
     rainRoofs: rainRoofsReducer,
-    rainRunoffs: rainRunoffsReducer,
+    // rainRunoffs: rainRunoffsReducer,
     users: usersReducer,
     types: typesReducer,
+    blocks: blocksReducer,
     [projectsApi.reducerPath]: projectsApi.reducer,
     [documentsApi.reducerPath]: documentsApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [typesApi.reducerPath]: typesApi.reducer,
+    [blocksApi.reducerPath]: blocksApi.reducer,
+    [rainRoofsApi.reducerPath]: rainRoofsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(
@@ -38,6 +44,8 @@ export const store = configureStore({
       documentsApi.middleware,
       usersApi.middleware,
       typesApi.middleware,
+      blocksApi.middleware,
+      rainRoofsApi.middleware,
     ),
 });
 
--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -1,6 +1,7 @@
 export * from './documents-slice';
 export * from './projects-slice';
-export * from './rain-roofs-slice';
+// export * from './rain-roofs-slice';
 export * from './rain-runoffs-slice';
 export * from './users-slice';
 export * from './types-slice';
+export * from './blocks-slice';
