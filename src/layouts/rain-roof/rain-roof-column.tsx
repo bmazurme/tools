0a847d4954e24e0058225ba011fd121626f3366b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-06 09:56:25 +0300

    fix: rain-roof-column

--- src/layouts/rain-roof/rain-roof-column.tsx
+++ src/layouts/rain-roof/rain-roof-column.tsx
@@ -7,14 +7,16 @@ import Column from '../../components/column/column';
 // import { useAppDispatch } from '../../hooks';
 // import { addRainRoofItem } from '../../store';
 import { TARGET_TYPE } from '../../config';
+import { useCreateItemMutation } from '../../store';
 
 type ColumnType = {
   children: ReactNode;
   blockId: number;
+  length: number;
 };
 
-export default function RainRoofColumn({ children, blockId }: ColumnType) {
-  // const dispatch = useAppDispatch();
+export default function RainRoofColumn({ children, blockId, length }: ColumnType) {
+  const [createItem] = useCreateItemMutation();
   const [{ isOver, canDrop }, drop] = useDrop({
     accept: TARGET_TYPE.ITEMS,
     drop: () => ({ blockId }),
@@ -43,7 +45,7 @@ export default function RainRoofColumn({ children, blockId }: ColumnType) {
     }
   };
   const onHandleAddItem = () => {
-    // dispatch(addRainRoofItem({ blockId }));
+    createItem({ block: { id: blockId }, index: length });
   };
 
   return (
