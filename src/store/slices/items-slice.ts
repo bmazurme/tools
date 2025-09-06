ddd29a0c0d390aed5a782eb2df91caff5983c894
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-06 09:53:32 +0300

    add: items-slice

---
+++ src/store/slices/items-slice.ts
@@ -0,0 +1,67 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+import { itemsApiEndpoints } from '../api/items-api/endpoints/index';
+import { type RootState } from '..';
+
+export type ItemsState = {
+  data: {
+    items: ItemType[];
+  };
+};
+
+export const initialStateItems: ItemsState = {
+  data: {
+    items: [],
+  },
+};
+
+const slice = createSlice({
+  name: 'items',
+  initialState: initialStateItems,
+  reducers: { },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(
+        itemsApiEndpoints.endpoints.getItems.matchFulfilled,
+        (state, { payload }: PayloadAction<ItemType[]>) => ({
+          ...state,
+          data: { items: payload },
+        }),
+      )
+      .addMatcher(
+        itemsApiEndpoints.endpoints.refreshItems.matchFulfilled,
+        (state, { meta }) => ({
+          ...state,
+          data: { items: meta.arg.originalArgs.data },
+        }),
+      )
+      .addMatcher(
+        itemsApiEndpoints.endpoints.updateItem.matchFulfilled,
+        (state, { meta }) => ({
+          ...state,
+          data: {
+            items: state.data.items.map((x) => (x.id === meta.arg.originalArgs.id
+              ? { ...meta.arg.originalArgs }
+              : x)),
+          },
+        }),
+      )
+      .addMatcher(
+        itemsApiEndpoints.endpoints.deleteItem.matchFulfilled,
+        (state, { meta }) => ({
+          ...state,
+          data: { items: state.data.items.filter((x) => x.id !== meta.arg.originalArgs) },
+        }),
+      )
+      .addMatcher(
+        itemsApiEndpoints.endpoints.createItem.matchFulfilled,
+        (state, { payload }: PayloadAction<ItemType>) => ({
+          ...state,
+          data: { items: [...state.data.items, payload] },
+        }),
+      );
+  },
+});
+
+export default slice.reducer;
+export const itemsSelector = (state: RootState) => state.items.data;
