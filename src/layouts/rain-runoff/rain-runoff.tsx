0a92def5a294c64165206b179f5594dd6a3f3794
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-11 07:59:19 +0300

    fix: rain-runoff

--- src/layouts/rain-runoff/rain-runoff.tsx
+++ src/layouts/rain-runoff/rain-runoff.tsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Board from './rain-runoff-board';
-import { useGetBlocksMutation } from '../../store';
+
+import { useGetBlocksMutation, useGetItemsMutation } from '../../store';
 
 export default function RainRunoffPage() {
   const { id } = useParams();
   const [getRainRoofs] = useGetBlocksMutation();
+  const [getItems] = useGetItemsMutation();
 
   useEffect(() => {
     if (id) {
       getRainRoofs(Number(id));
+      getItems(Number(id));
     }
   }, [getRainRoofs, id]);
 
