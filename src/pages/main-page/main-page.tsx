81ff7822231c93af66ef42acf63768d74a511fa9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-09-12 18:28:23 +0300

    fix: main-page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@gravity-ui/uikit';
+
 export default function MainPage() {
+  const navigate = useNavigate();
+  const handleProjects = () => navigate('/projects');
   return (
     <>
       Main
+      <Button view="flat" size="l" onClick={handleProjects}>
+        Проекты
+      </Button>
     </>
   );
 }
