0d74e07ca5b4f69617ff097df3396907bc6a7c1d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-31 17:38:43 +0300

    fix: add signin layout

--- src/layouts/signin-layout/signin-layout.tsx
+++ src/layouts/signin-layout/signin-layout.tsx
@@ -1,20 +1,41 @@
-import { Button, Text } from '@gravity-ui/uikit';
 import { useNavigate } from 'react-router-dom';
+import {
+  Button, Card, Text,
+} from '@gravity-ui/uikit';
 
 import style from './signin-layout.module.css';
 
 export default function SigninLayout() {
   const navigate = useNavigate();
+
   return (
     <div className={style.container}>
-      <Text variant="header-1">Sign in</Text>
-      <Button
-        view="normal-contrast"
-        size="l"
-        onClick={() => navigate('/projects')}
-      >
-        Normal Contrast
-      </Button>
+      <div className={style.wrapper}>
+        <Card
+          style={style}
+          view="filled"
+          type="container"
+          size="l"
+          className={style.card}
+        >
+          <div className={style.header}>
+            <Text variant="header-2">
+              Tools
+            </Text>
+          </div>
+          <div className={style.form}>
+            <Text
+              variant="header-1"
+              className={style.title}
+            >
+              Sign in
+            </Text>
+            <a href="https://oauth.yandex.ru/authorize?response_type=code&client_id=2f47d503901842298d06b55c9ba625b4">
+              Войдите с Яндекс ID
+            </a>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
