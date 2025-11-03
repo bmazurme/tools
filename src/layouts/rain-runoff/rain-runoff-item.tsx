2047bde01c6b19bc5a59e6a9a1e365cc22494978
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-03 13:25:43 +0300

    fix: rain-runoff-item

--- src/layouts/rain-runoff/rain-runoff-item.tsx
+++ src/layouts/rain-runoff/rain-runoff-item.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useRef } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
+import { Controller, useForm } from 'react-hook-form';
+import { TextInput, Text } from '@gravity-ui/uikit';
+
 import Item from '../../components/item/item';
 
 import { TARGET_TYPE } from '../../config';
@@ -11,12 +15,34 @@ import {
   useUpdateItemMutation,
 } from '../../store';
 
+import style from './rain-runoff-column.module.css';
+
+type FormPayload = { name: string };
+
+const fields = [
+  {
+    name: 'name',
+    // label: 'Название',
+    placeholder: 'Название',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{3,50}$/,
+      message: 'Name is invalid',
+    },
+    required: 'Обязательно к заполнению',
+    autoComplete: 'name',
+  },
+];
+
 export default function RainRunoffItem({ item, index }: { item: ItemType; index: number }) {
   const { items } = useAppSelector(itemsSelector) ?? { items: [] };
   const [updateItem] = useUpdateItemMutation();
   const [deleteItem] = useDeleteItemMutation();
   const [refreshItems] = useRefreshItemsMutation();
   const ref = useRef<HTMLLIElement>(null);
+
+  const { control } = useForm<FormPayload>({
+    defaultValues: { name: item.name },
+  });
 
   const moveCardHandler = async (dragIndex: number, hoverIndex: number, it: ItemType) => {
     const blockItems = items.filter((x) => x.column === it.column);
@@ -95,10 +121,48 @@ export default function RainRunoffItem({ item, index }: { item: ItemType; index:
   const opacity = isDragging ? 0.4 : 1;
   drag(drop(ref));
 
+  const onSubmit = async () => {
+    try {
+      // eslint-disable-next-line no-underscore-dangle
+      const { name } = control._formValues;
+
+      if (item.name !== name) {
+        await updateItem({ ...item, name });
+      }
+    } catch (error) {
+      console.error('Ошибка при обновлении проекта:', error);
+    }
+  };
+
   return (
     <li ref={ref} className="item" style={{ opacity }}>
       <Item removeAction={onHandleRemoveItem} editAction={() => {}}>
-        {`${item.id} - item - ${item.name}`}
+        <ul className="fields">
+          <Text variant="code-1" className={style.id}>{item.index + 1}</Text>
+          {fields.map((input) => (
+            <Controller
+              key={input.name}
+              name={input.name as keyof FormPayload}
+              rules={{
+                pattern: input.pattern,
+                required: input.required,
+              }}
+              control={control}
+              render={({ field, fieldState }) => (
+                <TextInput
+                  {...field}
+                  {...input}
+                  size="m"
+                  type="text"
+                  error={fieldState.error?.message}
+                  className={style.name}
+                  onBlur={onSubmit}
+                />
+              )}
+            />
+          ))}
+          <Text variant="code-1" className={style.id}>{item.index + 1}</Text>
+        </ul>
       </Item>
     </li>
   );
