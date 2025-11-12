5e7686147acbe0a3cb9331c3d915b2ec1f15175b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-12 09:15:46 +0300

    fix: main-page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,9 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@gravity-ui/uikit';
+import { Button, Card } from '@gravity-ui/uikit';
 
 export default function MainPage() {
   const navigate = useNavigate();
-  const handleProjects = () => navigate('/projects');
+  const handleProjects = () => navigate('/signin');
+  const style = {
+    width: '320px',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
   return (
     <>
       Main
@@ -12,8 +19,14 @@ export default function MainPage() {
       Hic, atque, quia sunt consectetur eius corrupti,
       expedita sapiente exercitationem aperiam quibusdam libero ipsa veritatis quisquam!
       <Button view="flat" size="l" onClick={handleProjects}>
-        Проекты
+        Войти
       </Button>
+      <Card style={style} theme="normal" size="l">Водоснабжение</Card>
+      <Card style={style} theme="info" size="l">Водоотведение</Card>
+      <Card style={style} theme="success" size="l">Success</Card>
+      <Card style={style} theme="warning" size="l">Warning</Card>
+      <Card style={style} theme="danger" size="l">Danger</Card>
+      <Card style={style} theme="utility" size="l">Utility</Card>
     </>
   );
 }
