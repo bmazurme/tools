1fcf7bb3b1f2a8eab169468270daf6e84aee75e4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-12 18:46:05 +0300

    fix: column-footer

--- src/components/column/column-footer.tsx
+++ src/components/column/column-footer.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 export default function ColumnFooter({ children }:
   { children: ReactNode; }) {
   return (
-    <div className="column_header">
+    <div className="column_footer">
       {children}
     </div>
   );
