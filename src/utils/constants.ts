ba1982e3a02357497ceaabe425cdbc6f343f0fa5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-12 09:14:39 +0300

    fix: constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,15 @@
 // eslint-disable-next-line import/prefer-default-export
 export const NUMBER_PATTERN = /^-?\d+(\.\d+)?$/;
+export const LATEX = {
+  q5: '$q_{5}$',
+  q20: '$q_{20}$',
+  n: '$n$',
+  Qr: '$Q_{r}$',
+  tr: '$t_{r}$',
+  Fr: '$F_{r}$',
+  lcan: '$l_{can}$',
+  lp: '$l_{p}$',
+  vcan: '$v_{can}$',
+  vp: '$v_{p}$',
+  tcon: '$t_{con}$',
+};
