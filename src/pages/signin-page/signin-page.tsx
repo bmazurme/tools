84507da5a9dff2e646d593b75a80ca5272d03162
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-12 18:54:44 +0300

    fix: signin-page

--- src/pages/signin-page/signin-page.tsx
+++ src/pages/signin-page/signin-page.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import SigninLayout from '../../layouts/signin-layout/signin-layout';
 import withUser from '../../hocs/with-user';
+import useUser from '../../hooks/use-user';
 
 function SignInPage() {
+  const navigate = useNavigate();
+  const user = useUser();
+
+  useEffect(() => {
+    if (user) {
+      navigate('/projects', { replace: true });
+    }
+  }, [user, navigate]);
+
   return (<SigninLayout />);
 }
 
