f1eaff2a029cfc6743a584881e7ce0535b29623c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-22 20:34:45 +0300

    fix: app

--- src/app.tsx
+++ src/app.tsx
@@ -15,40 +15,59 @@ import DocumentAddPage from './pages/document-add-page';
 import DocumentPage from './pages/document-page/document-page';
 import ProfilePage from './pages/profile-page';
 
+import RainRunoffDetailPage from './pages/rain-runoff-details-page/rain-runoff-details-page';
+
+import { useAppLocation } from './hooks/use-app-location';
+
 import './app.css';
 
 function App() {
+  const location = useAppLocation();
+  const background = location.state?.pathname;
+
   return (
-    <Routes>
-      <Route path="projects/:pageId?" element={(<ProjectsPage />)} />
-      <Route path="projects/add" element={(<ProjectAddPage />)} />
+    <>
+      <Routes location={background || location}>
+        <Route path="projects/:pageId?" element={(<ProjectsPage />)} />
+        <Route path="projects/add" element={(<ProjectAddPage />)} />
 
-      <Route
-        path="project"
-        element={<Navigate to="/projects" replace />}
-      />
-      <Route path="project/:projectId" element={(<DocumentsPage />)} />
-      <Route path="project/:projectId/add" element={(<DocumentAddPage />)} />
-      <Route path="project/:projectId/edit" element={(<ProjectEditPage />)} />
-
-      <Route path="project/:projectId" element={(<ProjectPage />)}>
         <Route
-          path="document"
-          element={<Navigate to=".." replace />}
+          path="project"
+          element={<Navigate to="/projects" replace />}
         />
-        <Route path="document/:id" element={(<DocumentPage />)}>
-          <Route path="rain-roof" element={(<RainRoof />)} />
-          <Route path="rain-runoff" element={(<RainRunoff />)} />
+        <Route path="project/:projectId" element={(<DocumentsPage />)} />
+        <Route path="project/:projectId/add" element={(<DocumentAddPage />)} />
+        <Route path="project/:projectId/edit" element={(<ProjectEditPage />)} />
+
+        <Route path="project/:projectId" element={(<ProjectPage />)}>
+          <Route
+            path="document"
+            element={<Navigate to=".." replace />}
+          />
+          <Route path="document/:id" element={(<DocumentPage />)}>
+            <Route path="rain-roof" element={(<RainRoof />)} />
+            <Route path="rain-runoff" element={(<RainRunoff />)} />
+          </Route>
         </Route>
-      </Route>
 
-      <Route path="profile" element={(<ProfilePage />)} />
-      <Route path="settings" element={(<SettingsPage />)} />
+        <Route path="profile" element={(<ProfilePage />)} />
+        <Route path="settings" element={(<SettingsPage />)} />
 
-      <Route path="/" element={(<MainPage />)} />
-      <Route path="signin" element={(<SigninPage />)} />
-      <Route path="*" element={(<NotFoundPage />)} />
-    </Routes>
+        <Route path="/" element={(<MainPage />)} />
+        <Route path="signin" element={(<SigninPage />)} />
+        <Route path="*" element={(<NotFoundPage />)} />
+      </Routes>
+
+      {background
+        && (
+          <Routes>
+            <Route
+              path="project/:projectId/document/:id/rain-runoff/details/:itemId"
+              element={(<RainRunoffDetailPage />)}
+            />
+          </Routes>
+        )}
+    </>
   );
 }
 
