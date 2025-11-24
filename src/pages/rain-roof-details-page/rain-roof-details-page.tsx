90be4738c18de620d9be893acc92c14cf22d22b6
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-24 21:45:12 +0300

    fix: rain-roof

--- src/pages/rain-roof-details-page/rain-roof-details-page.tsx
+++ src/pages/rain-roof-details-page/rain-roof-details-page.tsx
@@ -1,13 +1,10 @@
 import { useParams } from 'react-router-dom';
-import { Modal } from '@gravity-ui/uikit';
 
 export default function RainRoofDetailPage() {
   const { itemId } = useParams<{ itemId: string }>();
   console.log(itemId);
 
   return (
-    <Modal open disableOutsideClick>
-      <div>tmp</div>
-    </Modal>
+    <div>{itemId}</div>
   );
 }
