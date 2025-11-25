126190f95594ec2c6a444b89814d4f01b771a0d9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-11-25 18:42:51 +0300

    fix: rain-roof layout

---
+++ src/layouts/rain-roof-details-layout/rain-roof-details-page.tsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+export default function RainRoofTemplate() {
+  const { itemId } = useParams<{ itemId: string }>();
+  console.log(itemId);
+
+  return (
+    <div>{itemId}</div>
+  );
+}
