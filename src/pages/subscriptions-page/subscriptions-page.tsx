a6699b427a070743595dbbfc48152fdaad2b669c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-07 16:17:13 +0300

    fix: subscriptions

--- src/pages/subscriptions-page/subscriptions-page.tsx
+++ src/pages/subscriptions-page/subscriptions-page.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@gravity-ui/uikit';
+import { Text, Button } from '@gravity-ui/uikit';
 
 import Content from '../../components/content/content';
 
@@ -7,6 +7,16 @@ export default function SubscriptionsPage() {
     <Content sidebar>
       <div className="content">
         <Text variant="header-1">Подписка</Text>
+        <Text variant="body-1">
+          Main
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Voluptates asperiores accusamus est, ab rerum harum hic delectus fuga veniam!
+          Hic, atque, quia sunt consectetur eius corrupti,
+          expedita sapiente exercitationem aperiam quibusdam libero ipsa veritatis quisquam!
+        </Text>
+        <Button view="outlined-info" size="l">Outlined Info</Button>
+        <Button view="outlined-success" size="l">Outlined Success</Button>
+        <Button view="outlined-warning" size="l">Outlined Warning</Button>
       </div>
     </Content>
   );
