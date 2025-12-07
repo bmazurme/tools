3cf3330e778adac737f63d8ee404e23946e0837e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-07 13:18:24 +0300

    add: subscriptions page

---
+++ src/pages/subscriptions-page/subscriptions-page.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@gravity-ui/uikit';
+
+import Content from '../../components/content/content';
+
+export default function SubscriptionsPage() {
+  return (
+    <Content sidebar>
+      <div className="content">
+        <Text variant="header-1">Подписка</Text>
+      </div>
+    </Content>
+  );
+}
