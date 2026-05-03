f03128abdca897923dc8563e8e6a9a298a39e10a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-03 21:38:44 +0300

    add: useAppToaster

---
+++ src/hooks/use-app-toaster.ts
@@ -0,0 +1,27 @@
+import { useToaster } from '@gravity-ui/uikit';
+
+const useAppToaster = () => {
+  const { add: addToast } = useToaster();
+
+  const showSuccess = (title: string, content: string) => {
+    addToast({
+      title,
+      content,
+      theme: 'success',
+      name: 'success',
+    });
+  };
+
+  const showError = (error: string | Error, customTitle = 'Ошибка при загрузке данных') => {
+    addToast({
+      title: customTitle,
+      content: `${error}`,
+      theme: 'danger',
+      name: 'error',
+    });
+  };
+
+  return { showSuccess, showError };
+};
+
+export default useAppToaster;
