788410793d77077b806dd5890831485d62cd1706
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-04 16:48:55 +0300

    fix: Profile page

--- src/pages/profile-page/index.ts
+++ src/pages/profile-page/index.ts
@@ -1,4 +1,3 @@
-import withUser from '../../hocs/with-user';
 import ProfilePage from './profile-page';
 
-export default withUser(ProfilePage, true) as typeof ProfilePage;
+export default ProfilePage;
---
+++ src/pages/profile-page/profile-layout.tsx
@@ -0,0 +1,197 @@
+/* eslint-disable react/no-children-prop */
+/* eslint-disable react/jsx-props-no-spreading */
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Controller, useForm } from 'react-hook-form';
+import {
+  Button, Text, TextInput, Icon, Checkbox,
+} from '@gravity-ui/uikit';
+import { Circle } from '@gravity-ui/icons';
+
+import {
+  activitiesSelector, setActivities, useGetActivitiesMutation,
+  useGetStatusMutation, useUpdateUserMutation,
+} from '../../store';
+import useUser from '../../hooks/use-user';
+import useAppToaster from '../../hooks/use-app-toaster';
+import { TEXT_INPUT_PROPS } from '../../config';
+
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { setStatus } from '../../store/slices/subscriptions-slice';
+import LayoutWrapper from '../../components/layout-wrapper/layout-wrapper';
+
+import style from './profile.module.css';
+
+type FormPayload = { status: string; email: string };
+
+const fields = [
+  {
+    name: 'email',
+    label: 'Email',
+    disabled: true,
+  },
+  {
+    name: 'status',
+    label: 'Статус',
+    pattern: {
+      value: /^[A-Za-zА-Яа-я0-9., -]{0,50}$/,
+      message: 'Name is invalid',
+    },
+  },
+];
+
+export default function ProfileLayout() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const activities = useAppSelector(activitiesSelector);
+  const { showSuccess, showError } = useAppToaster();
+
+  const [updateUser] = useUpdateUserMutation();
+  const [getActivities, { isLoading: isLoadingActivities }] = useGetActivitiesMutation();
+  const [getStatus, { isLoading: isLoadingStatus }] = useGetStatusMutation();
+
+  const { control, handleSubmit } = useForm<FormPayload>({
+    defaultValues: {
+      status: user?.status || '',
+      email: user?.email || '',
+    },
+  });
+
+  const onSubmit = async (data: FormPayload) => {
+    try {
+      await updateUser({ ...user!, status: data.status });
+      showSuccess('Статус успешно обновлен', data.status);
+    } catch (error) {
+      showError(`${error}`, 'Ошибка при обновлении статуса');
+    }
+  };
+
+  const formatDate = (postgresStr: string, hoursOffset = 3) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      // timeZone: 'Europe/Moscow', // Укажите нужный IANA timezone ID
+    };
+
+    const date = new Date(postgresStr);
+    date.setHours(date.getHours() + hoursOffset);
+
+    return new Intl.DateTimeFormat('ru-RU', options)
+      .format(date)
+      .replace(/\./g, '.') // Гарантируем точку как разделитель
+      .replace(',', ', '); // Добавляем пробел после запятой
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        const [data1, data2] = await Promise.all([
+          getActivities(),
+          getStatus(),
+        ]);
+
+        if (!isMounted) return;
+
+        dispatch(setActivities({ activities: data1.data }));
+        dispatch(setStatus(data2.data));
+      } catch (error) {
+        // dispatch(setError({ error: error.message }));
+        showError(`${error}`);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [getActivities, getStatus, dispatch]);
+
+  return (
+    <LayoutWrapper isLoading={isLoadingActivities || isLoadingStatus}>
+      <form className="content" onSubmit={handleSubmit(onSubmit)}>
+        <div className="project_main">
+          <Text variant="header-1">
+            Профиль
+          </Text>
+        </div>
+        {fields.map((input) => (
+          <Controller
+            key={input.name}
+            name={input.name as keyof FormPayload}
+            rules={{
+              pattern: input.pattern,
+              // required: input.required,
+            }}
+            control={control}
+            render={({ field, fieldState }) => (
+              <TextInput
+                {...field}
+                {...input}
+                {...TEXT_INPUT_PROPS}
+                error={fieldState.error?.message}
+              />
+            )}
+          />
+        ))}
+
+        <div className="buttons">
+          <Button
+            view="action"
+            size="l"
+            type="submit"
+          >
+            Сохранить
+          </Button>
+        </div>
+      </form>
+
+      <div className="content">
+        <Text variant="header-1">
+          Подписка
+        </Text>
+        <Checkbox
+          size="l"
+          disabled
+          checked={user?.subscription}
+        >
+          Статус
+        </Checkbox>
+        <div className="buttons">
+          <Button
+            view="raised"
+            size="l"
+            onClick={() => navigate('/subscriptions')}
+          >
+            Продлить
+          </Button>
+        </div>
+      </div>
+
+      <div className="content">
+        <Text variant="header-1">
+          Активность
+        </Text>
+
+        <ul className={style.activities}>
+          {activities.map((x) => (
+            <li key={x.id} className={style.activity}>
+              <Icon data={Circle} />
+              <div className={style.description}>
+                {`${x.description} - ${formatDate(x.createdAt)}`}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </LayoutWrapper>
+  );
+}
