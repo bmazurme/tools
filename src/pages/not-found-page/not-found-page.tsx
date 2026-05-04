7e684afdcee7b582f7531317373ea638b85aedd0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-04 16:49:53 +0300

    fix: NotFound page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,14 @@
+import Content from '../../components/content/content';
 import NotFoundLayout from '../../layouts/not-found-layout';
 
 export default function NotFoundPage() {
   return (
-    <NotFoundLayout
-      title="404 — Страница не найдена"
-      description="К сожалению, запрошенная страница не существует."
-      buttonLabel="Вернуться на главную"
-    />
+    <Content>
+      <NotFoundLayout
+        title="404 — Страница не найдена"
+        description="К сожалению, запрошенная страница не существует."
+        buttonLabel="Вернуться на главную"
+      />
+    </Content>
   );
 }
