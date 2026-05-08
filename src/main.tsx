5f80bad455d577e135d273de6518085075833dff
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-08 17:30:42 +0300

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/function-component-definition */
 /* eslint-disable import/no-extraneous-dependencies */
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import { Toaster, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
 
 import ErrorBoundaryWrapper from './components/error-boundary-wrapper';
 import ThemeWrapper from './components/theme-wrapper/theme-wrapper';
 import App from './app';
+
 import { store } from './store';
 
 import '@gravity-ui/uikit/styles/fonts.css';
@@ -17,20 +21,35 @@ import './styles/index.css';
 
 const toaster = new Toaster();
 
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <ThemeWrapper>
-      <ToasterProvider toaster={toaster}>
-        <BrowserRouter>
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found. Please check your HTML file.');
+}
+
+const root = createRoot(rootElement);
+
+const AppWithProviders = () => (
+  <BrowserRouter>
+    <Provider store={store}>
+      <ThemeWrapper>
+        <ToasterProvider toaster={toaster}>
           <ErrorBoundaryWrapper>
             <App />
           </ErrorBoundaryWrapper>
           <ToasterComponent />
-        </BrowserRouter>
-      </ToasterProvider>
-    </ThemeWrapper>
-  </Provider>
-  // </StrictMode>
-  ,
+        </ToasterProvider>
+      </ThemeWrapper>
+    </Provider>
+  </BrowserRouter>
 );
+
+if (import.meta.env.DEV) {
+  root.render(
+    <StrictMode>
+      <AppWithProviders />
+    </StrictMode>,
+  );
+} else {
+  root.render(<AppWithProviders />);
+}
