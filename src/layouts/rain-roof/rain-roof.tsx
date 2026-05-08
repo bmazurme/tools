fdb648fc123d326cf2e01be327a78c2206fbce6d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-08 17:29:05 +0300

    fix: RainRoofLayout

--- src/layouts/rain-roof/rain-roof.tsx
+++ src/layouts/rain-roof/rain-roof.tsx
@@ -9,7 +9,6 @@ import { useAppSelector } from '../../hooks';
 export default function RainRoofLayout() {
   const { id, typeId } = useParams();
   const document = useAppSelector(documentSelector);
-  // console.log(document);
   const [getBlock] = useGetBlocksMutation();
   const [getItems] = useGetRainRoofsItemsMutation();
 
