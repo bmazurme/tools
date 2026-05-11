3d283d4a2339797deb3de460d1a0d8b269215660
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-11 11:08:03 +0300

    fix: RainRunoffLayout

--- src/layouts/rain-runoff/rain-runoff.tsx
+++ src/layouts/rain-runoff/rain-runoff.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Skeleton } from '@gravity-ui/uikit';
 
 import Board from './rain-runoff-board';
 
 import { documentSelector, useGetBlocksMutation, useGetRainRunoffsItemsMutation } from '../../store';
 import { useAppSelector } from '../../hooks';
+
+import style from './rain-runoff.module.css';
 
 export default function RainRunoffLayout() {
   const { id, typeId } = useParams();
@@ -21,7 +24,7 @@ export default function RainRunoffLayout() {
 
   return (
     <div className="gapb">
-      {document?.type.link !== typeId ? <>error</> : <Board />}
+      {document?.type.link !== typeId ? <Skeleton className={style.loader} /> : <Board />}
     </div>
   );
 }
