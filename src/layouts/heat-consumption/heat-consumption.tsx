0372002e950d045dc58515b732c780b5fd01e657
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-15 08:45:57 +0300

    fix: style

--- src/layouts/heat-consumption/heat-consumption.tsx
+++ src/layouts/heat-consumption/heat-consumption.tsx
@@ -10,8 +10,6 @@ import {
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import useAppToaster from '../../hooks/use-app-toaster';
 
-import style from './heat-consumption.module.css';
-
 export default function HeatConsumptionLayout() {
   const { showError } = useAppToaster();
   const dispatch = useAppDispatch();
@@ -39,7 +37,7 @@ export default function HeatConsumptionLayout() {
 
   return (
     <div className="gapb">
-      {document?.type.link !== typeId ? <Skeleton className={style.loader} /> : <Board />}
+      {document?.type.link !== typeId ? <Skeleton className="layout-loader" /> : <Board />}
     </div>
   );
 }
