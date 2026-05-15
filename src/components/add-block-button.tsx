d1f827048344e14937bb7b99c600713d8c7e57be
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-15 13:03:21 +0300

    add: AddBlockButton

---
+++ src/components/add-block-button.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@gravity-ui/uikit';
+
+interface AddBlockButtonProps {
+  onHandleAddBlock: () => void;
+  isCreatingBlock: boolean;
+}
+
+export default function AddBlockButton({ onHandleAddBlock, isCreatingBlock }
+  :AddBlockButtonProps) {
+  return (
+    <Button
+      view="action"
+      size="m"
+      title="Добавить блок"
+      onClick={onHandleAddBlock}
+      loading={isCreatingBlock}
+      disabled={isCreatingBlock}
+    >
+      Добавить блок
+    </Button>
+  );
+}
