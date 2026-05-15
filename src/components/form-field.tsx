aa49100a54f54d10f850693b609f55c3c1f0c63f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-15 13:04:18 +0300

    add: FormField

---
+++ src/components/form-field.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Controller, type Control } from 'react-hook-form';
+import { TextInput } from '@gravity-ui/uikit';
+
+interface FieldConfig {
+  name: string;
+  label: string;
+  required?: string;
+  pattern?: {
+    value: RegExp;
+    message: string;
+  };
+  placeholder?: string;
+}
+
+interface FormFieldProps {
+  fieldConfig: FieldConfig;
+  control: Control<any>;
+}
+
+export default function FormField({ fieldConfig, control }: FormFieldProps) {
+  return (
+    <Controller
+      key={fieldConfig.name}
+      name={fieldConfig.name as string}
+      rules={{
+        pattern: fieldConfig.pattern,
+        required: fieldConfig.required,
+      }}
+      control={control}
+      render={({ field, fieldState }) => (
+        <TextInput
+          {...field}
+          {...fieldConfig}
+          value={`${field.value}`}
+          size="l"
+          type="text"
+          error={fieldState.error?.message}
+          label={fieldConfig.label}
+        />
+      )}
+    />
+  );
+}
