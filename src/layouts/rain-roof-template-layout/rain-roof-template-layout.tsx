0803a694cce58a56c7d37f02d25c39ddcdd8020c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-13 11:41:11 +0300

    fix: RainRoof showError

--- src/layouts/rain-roof-template-layout/rain-roof-template-layout.tsx
+++ src/layouts/rain-roof-template-layout/rain-roof-template-layout.tsx
@@ -27,8 +27,7 @@ export default function RainRoofTemplateLayout() {
         const data = await getRainRoofItem(+id).unwrap();
         dispatch(setRainRoofs({ item: data }));
       } catch (error) {
-        const message = error instanceof Error ? error.message : 'Неизвестная ошибка';
-        showError(message, 'Ошибка');
+        showError(error, 'Ошибка');
       }
     };
 
